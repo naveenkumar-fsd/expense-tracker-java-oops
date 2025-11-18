af4c31565e52fea376d6868ebc1ccdd6867c4561
naveenkumar-fsd <naveenkumar977912345@gmail.com>
2025-11-18 17:04:47 +0530

    Add files via upload

---
+++ CSVUtil.java
@@ -0,0 +1,39 @@
+package com.expense.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import com.expense.model.Expense;
+
+public class CSVUtil {
+	
+	//CSV:Comma Separated Values-it is a file used by Excel,Google sheets,Data Analysis Tools....
+	
+	//Abstraction used : CSV export logic hidden in one reusable util method.
+
+    public static void exportToCSV(List<Expense> expenses) {
+        String fileName = "expenses.csv";
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+
+            // Writing table headings
+            writer.write("ID,Name,Amount,Date,Category\n");
+
+            // Write each expense as a CSV row
+            for (Expense e : expenses) {
+                writer.write(
+                	e.getId() + ","+	
+                    e.getName() + "," +
+                    e.getAmount() + "," +
+                    e.getDate() + "," +
+                    e.getCategory() + "\n"
+                );
+            }
+
+            System.out.println("CSV Export Successful");
+
+        } catch (IOException ex) {
+            System.out.println("Error writing CSV: " + ex.getMessage());
+        }
+    }
+}
