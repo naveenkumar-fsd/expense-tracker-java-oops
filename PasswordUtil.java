c33c9d9026001570634339acd2ba2d8db50dc651
naveenkumar-fsd <naveenkumar977912345@gmail.com>
2025-12-06 09:56:54 +0530

    Update PasswordUtil.java

--- PasswordUtil.java
+++ PasswordUtil.java
@@ -1,6 +1,8 @@
 package com.expense.password;
 
 //Abstraction used : password validation logic is hidden inside a separate class
+
+// NOTE: For real apps, don't hardcode password — use hashed credentials.
 public class PasswordUtil {
 	
 	private static final String PASSWORD="1234";
